e326d9a09237e7dda50940a20820b8be0d4bff4f
XiaoHan <810848107@qq.com>
2021-09-05 15:52:01 +0800

    second commit

--- src/main/java/com/lixiaohan/git/GitTest.java
+++ src/main/java/com/lixiaohan/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lixiaohan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello!git");
+        System.out.println("git2");
     }
 }
